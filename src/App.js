484c23032264ba945e97ac7d1182ce9f588cc223
TakahiroToyaGit <n10056513@qut.edu.au>
2020-04-27 11:38:50 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./styles/HomeStyle.css";
+import "./styles/StocksStyle.css";
+
+// components
+import Header from "./components/Header";
+
+// pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Stocks from "./pages/Stocks";
+import PriceHistory from "./pages/PriceHistory";
+import Quote from "./pages/Quote";
+import Register from "./pages/Register";
+import useSymbols from "./api.js";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/price-hisotry">
+            <PriceHistory />
+          </Route>
+          <Route exact path="/quote">
+            <Quote />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/stocks">
+            <Stocks />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
